3ac53c55430c469a6f8778a7555095dd7b7ab06c
bshin132 <bshin132@gmail.com>
2022-03-10 04:11:44 +0000

    implement dashboard and style

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PW']
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
 end

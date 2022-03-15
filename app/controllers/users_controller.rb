38c14c7a1f37318e009479ae793d3dcec179be19
bshin132 <bshin132@gmail.com>
2022-03-15 23:58:47 +0000

    successfully test product show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.error.full_messages 
-      session[:error] = @user.error
-    end
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/'
     else
+      flash[:alert] = @user.errors.full_messages
       redirect_to '/signup'
     end
   end
